10e94b70c49255a1d35cd90af50d9a2c4fc98dc7
DESKTOP-0HCNIPI\ricardo <ricardobettonvil@gmail.com>
2023-05-31 10:27:46 +0200

    test

--- FaceRecognitionTraining/Program.cs
+++ FaceRecognitionTraining/Program.cs
@@ -1,5 +1,30 @@
 ﻿using FaceRecognitionTraining;
 
-Console.WriteLine("test");
+Main();
 
-VideoFeed x = new VideoFeed();
+static void Main()
+{
+    AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+
+    Console.WriteLine("test");
+
+    VideoFeed x = new VideoFeed();
+}
+
+static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+{
+    if (e.ExceptionObject is System.IO.FileLoadException fileLoadException)
+    {
+        // Handle the FileLoadException here
+        Console.WriteLine("FileLoadException occurred: " + fileLoadException.Message);
+        // Additional error handling or logging can be performed
+    }
+    else
+    {
+        // Handle other unhandled exceptions here
+        Console.WriteLine("Unhandled exception occurred: " + e.ExceptionObject.ToString());
+    }
+
+    // Terminate the application or perform any necessary cleanup
+    Environment.Exit(1);
+}
