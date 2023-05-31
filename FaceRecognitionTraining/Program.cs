784e13438744a332bc1f7d959a7b3d73185635c7
ricardo <ricardobettonvil@gmail.com>
2023-05-31 10:37:55 +0200

    kut leve

--- FaceRecognitionTraining/Program.cs
+++ FaceRecognitionTraining/Program.cs
@@ -1,30 +1,5 @@
 ﻿using FaceRecognitionTraining;
 
-Main();
+Console.WriteLine("test");
 
-static void Main()
-{
-    AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
-
-    Console.WriteLine("test");
-
-    VideoFeed x = new VideoFeed();
-}
-
-static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
-{
-    if (e.ExceptionObject is System.IO.FileLoadException fileLoadException)
-    {
-        // Handle the FileLoadException here
-        Console.WriteLine("FileLoadException occurred: " + fileLoadException.Message);
-        // Additional error handling or logging can be performed
-    }
-    else
-    {
-        // Handle other unhandled exceptions here
-        Console.WriteLine("Unhandled exception occurred: " + e.ExceptionObject.ToString());
-    }
-
-    // Terminate the application or perform any necessary cleanup
-    Environment.Exit(1);
-}
+VideoFeed x = new VideoFeed();
