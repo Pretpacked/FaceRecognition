6b895c1df78eb3185621fce68f59883d7ad7bc76
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2023-06-12 10:07:43 +0200

    Update over the weekend the weeeeekendddd

--- FaceRecognitionTraining/FaceDetector.cs
+++ FaceRecognitionTraining/FaceDetector.cs
@@ -1,30 +1,1 @@
-﻿using Emgu.CV.CvEnum;
-using Emgu.CV;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FaceRecognitionTraining
-{
-    public class FaceDetector
-    {
-        private CascadeClassifier cascade;
-        private string MyDocuments = Environment.GetFolderPath(
-            Environment.SpecialFolder.MyDocuments);
-
-        public FaceDetector()
-        {
-            this.cascade = new CascadeClassifier(
-                this.MyDocuments + "\\haarcascade_frontalface_alt.xml");
-        }
-
-        public System.Drawing.Rectangle[] DetectFaces(Mat frame)
-        {
-            Mat grayFrame = new Mat();
-            CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);
-            return cascade.DetectMultiScale(grayFrame, 1.4, 0);
-        }
-    }
-}
+﻿using Emgu.CV.CvEnum;using Emgu.CV;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace FaceRecognitionTraining{    public class FaceDetector    {        private CascadeClassifier cascade;        public string MyDocuments = Environment.GetFolderPath(            Environment.SpecialFolder.MyDocuments);        public FaceDetector()        {            this.cascade = new CascadeClassifier(                this.MyDocuments + "\\haarcascade_frontalface_alt.xml");        }        public System.Drawing.Rectangle[] DetectFaces(Mat frame)        {            Mat grayFrame = new Mat();            CvInvoke.CvtColor(frame, grayFrame, ColorConversion.Bgr2Gray);                        return cascade.DetectMultiScale(grayFrame, 1.1, 3);        }    }}
--- FaceRecognitionTraining/FaceRecognizerTrainer.cs
+++ FaceRecognitionTraining/FaceRecognizerTrainer.cs
@@ -13,11 +13,11 @@ namespace FaceRecognitionTraining
     {
         private List<Image<Gray, byte>> faceDatabase;
         private EigenFaceRecognizer recognizer;
-
-        public FaceRecognizerTrainer(List<Image<Gray, byte>> faceDatabase)
+        private FaceDetector detector;
+        public FaceRecognizerTrainer()
         {
-            this.faceDatabase = faceDatabase;
             this.recognizer = new EigenFaceRecognizer();
+            this.faceDatabase = this.LoadFaceDatabase();
         }
 
         public void Train()
@@ -32,6 +32,27 @@ namespace FaceRecognitionTraining
             }
 
             recognizer.Train(faces.ToArray(), labels.ToArray());
+        }
+
+        private List<Image<Gray, byte>> LoadFaceDatabase()
+        {
+            List<Image<Gray, byte>> faceImages = new List<Image<Gray, byte>>();
+
+            try
+            {
+                string path = detector.MyDocuments + "\\FaceDatabase";
+                string[] files = Directory.GetFiles(
+                    path, "*.jpg", System.IO.SearchOption.AllDirectories);
+
+                foreach (string image in files)
+                {
+                    Image<Gray, byte> faceImage = new Image<Gray, byte>(image);
+                    faceImages.Add(faceImage);
+                }
+            }
+            catch (Exception ex) { System.Console.WriteLine(ex); }
+                
+            return faceImages;
         }
     }
 }
